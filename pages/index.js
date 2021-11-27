2db744b73a2bcf93f8c634aac5bea5c798b4eb15
Kaique Ribeiro de Campos <kaique@forlogic.org>
2021-11-27 17:07:19 -0300

    chakra ui

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,37 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import { signIn, signOut, useSession } from 'next-auth/client';
-import { Redirect } from 'react-dom';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { signIn, signOut, useSession } from "next-auth/client";
+import { Spinner, Flex, Heading, Button, Box } from "@chakra-ui/react";
 
 export default function Home() {
   const [session, loadingSession] = useSession();
 
-  if(loadingSession) {
-    return <p>Loading...</p>
+  if (loadingSession) {
+    <main className={styles.main}>
+      <Heading>Exemplo Blog Seminário</Heading>
+      <Flex
+        flexWrap="wrap"
+        alignItems="center"
+        justifyContent="center"
+        maxW="800px"
+        mt="10"
+      >
+        <Box>
+          <Flex>
+            <Heading mr="10" as="h3">
+              Carregando...
+            </Heading>
+            <Spinner
+              thickness="4px"
+              speed="0.65s"
+              emptyColor="gray.200"
+              color="blue.500"
+              size="xl"
+            />
+          </Flex>
+        </Box>
+      </Flex>
+    </main>;
   }
   return (
     <div className={styles.container}>
@@ -16,18 +40,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-    {!session && (
-      <>
-        <button onClick={() => signIn()}></button>
-      </>
-    )}
+      {!session && (
+        <>
+          <Button onClick={() => signIn()}>Entrar</Button>
+        </>
+      )}
 
-    {session && (
-      <>
-
-      </>
-    )}
-
+      {session && (
+        <>
+          <Button onClick={() => signOut()}>Sair</Button>
+        </>
+      )}
     </div>
-  )
+  );
 }
